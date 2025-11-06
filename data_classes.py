751f1f25f7cf493edbb0841c0a4b32d057a4bd95
haikalrozaidi <binrozaidi.1@osu.edu>
2025-11-06 01:29:02 -0600

    Implemented data_classes that has Ingredient, Step class for data repr

---
+++ data_classes.py
@@ -0,0 +1,23 @@
+class Ingredient:
+    """Represents a recipe ingredient"""
+    def __init__(self, name, quantity, measurement_unit):
+        self.name = name
+        self.quantity = quantity
+        self.measurement_unit = measurement_unit
+    
+    def __repr__(self):
+        return f"Ingredient(name='{self.name}', quantity='{self.quantity}', measurement_unit='{self.measurement_unit}')"
+
+class Step:
+    """Represents a recipe step"""
+    def __init__(self, step_number, description, ingredients=None, tools=None, methods=None, time=None, temperature=None):
+        self.step_number = step_number
+        self.description = description
+        self.ingredients = ingredients if ingredients is not None else []
+        self.tools = tools if tools is not None else []
+        self.methods = methods if methods is not None else []
+        self.time = time if time is not None else {}
+        self.temperature = temperature if temperature is not None else {}
+    
+    def __repr__(self):
+        return f"Step(step_number={self.step_number}, description='{self.description[:50]}...', ingredients={self.ingredients}, tools={self.tools})"
